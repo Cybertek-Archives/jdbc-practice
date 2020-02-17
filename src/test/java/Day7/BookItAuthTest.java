53d38d4b4e1a3b5c3482e9bebd1a5cff32e63203
cybertek <github@cybertekschool.com>
2020-02-17 15:56:31 -0500

    Day7 done

---
+++ src/test/java/Day7/BookItAuthTest.java
@@ -0,0 +1,30 @@
+package Day7;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.*;
+
+public class BookItAuthTest {
+
+    String accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4NiIsImF1ZCI6InN0dWRlbnQtdGVhbS1sZWFkZXIifQ.lEfjcu6RpBfcZ4qWthzZU8uH8fX4FCJFfxBnPNgh4Mo";
+    @BeforeClass
+    public void setUp(){
+        RestAssured.baseURI = "https://cybertek-reservation-api-qa3.herokuapp.com/api";
+    }
+
+    @Test
+    public void getAllcampusWithAccessToken(){
+        Response response = given().header("Authorization", accessToken)
+                .when().get("/campuses");
+
+        //assertEquals(response.statusCode(),200);
+        System.out.println(response.prettyPrint());
+
+
+    }
+}
--- src/test/java/Day7/SpartanPutDelete.java
+++ src/test/java/Day7/SpartanPutDelete.java
@@ -1,8 +1,15 @@
 package Day7;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
 
@@ -11,6 +18,34 @@ public class SpartanPutDelete {
     @BeforeClass
     public void setUp(){
         RestAssured.baseURI = ConfigurationReader.get("spartanapi.uri");
+    }
+
+    @Test
+    public void updatingSpartan(){
+        Map<String,Object> putMap = new HashMap<>();
+        putMap.put("name","putname");
+        putMap.put("gender","Female");
+        putMap.put("phone",6986436734L);
+
+            given().pathParam("id",50)
+                    .and().contentType(ContentType.JSON)
+                    .and().body(putMap)
+                    .when().put("/spartans/{id}").then()
+                    .assertThat().statusCode(204);
+    }
+
+    @Test
+
+    public void DeleteASpartan(){
+        Random rn=new Random();
+        int idToDelete=rn.nextInt(400)+2;
+        System.out.println("idToDelete = " + idToDelete);
+        given().pathParam("id",idToDelete)
+                .when().delete("/spartans/{id}").then()
+                .assertThat().statusCode(204);
+
+
+
     }
 
 }
